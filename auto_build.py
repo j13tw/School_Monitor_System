8af7e43ea5f3d237fe990127ffe54a20c281a312
j13tw <j13tw@yahoo.com.tw>
2018-12-03 03:15:47 +0800

    install is ok, next is uninstall

--- auto_build.py
+++ auto_build.py
@@ -31,16 +31,18 @@ def create_librenms(docker_librenms_name, docker_mysql_name, docker_mysql_ip, db
     print("安裝系統基礎監控服務")
     docker_librenms_config = "docker run -d -h " + docker_librenms_name + " -p 80:80 -e APP_KEY=" + librenms_product_key +  " -e DB_HOST='" + docker_mysql_ip + "' -e DB_NAME=" + db_name + " -e DB_USER=" + db_user_name + " -e DB_PASS=" + db_user_pwd + " -e BASE_URL=http://127.0.0.1" + " --link " + docker_mysql_name + ":db -v " + librenms_volume + "/librenms/logs:/opt/librenms/logs -v " + librenms_volume + "/librenms/rrd:/opt/librenms/rrd --name " + docker_librenms_name + " jarischaefer/docker-librenms"
 #    print(docker_librenms_config)
-    os.system(docker_librenms_config + "2&>1")
-    time.sleep(40)
+    os.system(docker_librenms_config + " >/dev/null 2>&1")
+    time.sleep(30)
+    print("監控系統服務啟動測試")
+    time.sleep(30)
     print("設定監控系統基礎資料")
     docker_librenms_config = "docker exec " + docker_librenms_name + " setup_database"
 #    print(docker_librenms_config)
-    os.system(docker_librenms_config + "2&>1")
+    os.system(docker_librenms_config + " >/dev/null 2>&1")
     print("創建最高權限使用者")
     docker_librenms_config = "docker exec " + docker_librenms_name + " create_admin"
 #    print(docker_librenms_config)
-    os.system(docker_librenms_config + "2&>1")
+    os.system(docker_librenms_config + " >/dev/null 2>&1")
 
 def create_mysql(docker_mysql_name, db_root_pwd, db_name, db_user_name, db_user_pwd):
     print("建立監控系統資料庫")
@@ -51,10 +53,10 @@ def create_mysql(docker_mysql_name, db_root_pwd, db_name, db_user_name, db_user_
 #    print(mysql_volume)
     docker_mysql_config = 'docker run --name ' + docker_mysql_name + " -d -e MYSQL_ROOT_PASSWORD=" + db_root_pwd + " -e MYSQL_USER=" + db_user_name + " -e MYSQL_PASSWORD=" + db_user_pwd + " -e MYSQL_DATABASE=" + db_name + " -p 127.0.0.1:3306:3306 -v " + mysql_volume + '/mysql:/var/lib/mysql mysql:5.6 --sql-mode=""'
 #    print(docker_mysql_config)
-    os.system(docker_mysql_config + "2&>1")
+    os.system(docker_mysql_config + " >/dev/null 2>&1")
     docker_mysql_config = 'docker exec -ti ' + docker_mysql_name + " service mysql start"
 #    print(docker_mysql_config)
-    os.system(docker_mysql_config + "2&>1")
+    os.system(docker_mysql_config + " >/dev/null 2>&1")
     docker_mysql_config = 'docker exec -ti ' + docker_mysql_name + " sh -c " + '"hostname -I"'
 #    print(docker_mysql_config)
     docker_mysql_ip = str(subprocess.Popen(docker_mysql_config, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].decode('utf-8')).split("\n")[0]
@@ -69,17 +71,23 @@ def create_service():
     librenms_mysql_user_db = school_name + "-db"
     librenms_mysql_user_name = school_name + "-user"
     librenms_mysql_user_pwd = school_name + "-pwd"
-    print("librenms_docker_name ", librenms_docker_name)
-    print("librenms_docker_mysql_name ", librenms_mysql_docker_name)
-    print("librenms_mysql_root_pwd ", librenms_mysql_root_pwd)
-    print("librenms_mysql_user_db ", librenms_mysql_user_db)
-    print("librenms_mysql_user_name ", librenms_mysql_user_name)
-    print("librenms_mysql_user_pwd ", librenms_mysql_user_pwd)
+#    print("librenms_docker_name ", librenms_docker_name)
+#    print("librenms_docker_mysql_name ", librenms_mysql_docker_name)
+#    print("librenms_mysql_root_pwd ", librenms_mysql_root_pwd)
+#    print("librenms_mysql_user_db ", librenms_mysql_user_db)
+#    print("librenms_mysql_user_name ", librenms_mysql_user_name)
+#    print("librenms_mysql_user_pwd ", librenms_mysql_user_pwd)
     print("資料庫服務名稱：", librenms_mysql_docker_name)
     print("資料庫總管密碼：", librenms_mysql_root_pwd)
     librenms_mysql_ip = create_mysql(librenms_mysql_docker_name, librenms_mysql_root_pwd, librenms_mysql_user_db, librenms_mysql_user_name, librenms_mysql_user_pwd)
-    print(librenms_mysql_ip)
+#    print(librenms_mysql_ip)
     create_librenms(librenms_docker_name, librenms_mysql_docker_name, librenms_mysql_ip, librenms_mysql_user_db, librenms_mysql_user_name, librenms_mysql_user_pwd)
+    print("監控系統安裝完成")
+    print("請輸入 http://127.0.0.1/")
+    print("帳號 : admin")
+    print("密碼 : admin")
+    print("感謝您的安裝與使用~")
+
 
 #    user_name = input("請輸入使用者帳號：")
 #    user_pwd = input("請輸入使用者密碼：")
