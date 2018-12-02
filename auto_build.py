533845b1d91ab1fa538ee954e617a62c7b9e6270
j13tw <j13tw@yahoo.com.tw>
2018-12-02 20:23:00 +0800

    librenms auto install is ok ~

--- auto_build.py
+++ auto_build.py
@@ -18,8 +18,8 @@ def delete_service():
     print("delete")
 
 def create_librenms(docker_librenms_name, docker_mysql_name, docker_mysql_ip, db_name, db_user_name, db_user_pwd):
-    print("Create librenms")
-    print("Generate product key")
+    print("建立監控系統總服務")
+    print("生成系統認證金鑰")
     docker_librenms_config = "docker run --rm jarischaefer/docker-librenms generate_key"
     librenms_product_key = str(subprocess.Popen(docker_librenms_config, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].decode('utf-8')).split("\n")[0]
 #    print(librenms_product_key)
@@ -27,19 +27,23 @@ def create_librenms(docker_librenms_name, docker_mysql_name, docker_mysql_ip, db
     if (os.path.isdir("./librenms/logs") == 0): os.mkdir("./librenms/logs")
     if (os.path.isdir("./librenms/rrd") == 0): os.mkdir("./librenms/rrd")
     librenms_volume = str(subprocess.Popen("pwd", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].decode('utf-8')).split("\n")[0]
-    print(librenms_volume)
-    docker_librenms_config_A = "docker run -d -h " + docker_librenms_name + " -p 80:80 -e APP_KEY=" + librenms_product_key
-    docker_librenms_config_B = " -e DB_HOST=" + docker_mysql_ip + " -e DB_NAME=" + db_name + " -e DB_USER=" + db_user_name + " -e DB_PASS=" + db_user_pwd + " -e BASE_URL=127.0.0.1" + " --link " + docker_mysql_name + ":db -v "
-    docker_librenms_config_C = librenms_volume + "/librenms/logs:/opt/librenms/logs -v " + librenms_volume + "/librenms/rrd:/opt/librenms/rrd --name "
-    docker_librenms_config_D = docker_librenms_name + " jarischaefer/docker-librenms"
-    print(docker_librenms_config_A)
-    print(docker_librenms_config_B)
-    print(docker_librenms_config_C)
-    print(docker_librenms_config_D)
-    os.system(docker_librenms_config_A + docker_librenms_config_B + docker_librenms_config_C + docker_librenms_config_D)
+#    print(librenms_volume)
+    print("安裝系統基礎監控服務")
+    docker_librenms_config = "docker run -d -h " + docker_librenms_name + " -p 80:80 -e APP_KEY=" + librenms_product_key +  " -e DB_HOST='" + docker_mysql_ip + "' -e DB_NAME=" + db_name + " -e DB_USER=" + db_user_name + " -e DB_PASS=" + db_user_pwd + " -e BASE_URL=http://127.0.0.1" + " --link " + docker_mysql_name + ":db -v " + librenms_volume + "/librenms/logs:/opt/librenms/logs -v " + librenms_volume + "/librenms/rrd:/opt/librenms/rrd --name " + docker_librenms_name + " jarischaefer/docker-librenms"
+#    print(docker_librenms_config)
+    os.system(docker_librenms_config + "2&>1")
+    time.sleep(40)
+    print("設定監控系統基礎資料")
+    docker_librenms_config = "docker exec " + docker_librenms_name + " setup_database"
+#    print(docker_librenms_config)
+    os.system(docker_librenms_config + "2&>1")
+    print("創建最高權限使用者")
+    docker_librenms_config = "docker exec " + docker_librenms_name + " create_admin"
+#    print(docker_librenms_config)
+    os.system(docker_librenms_config + "2&>1")
 
 def create_mysql(docker_mysql_name, db_root_pwd, db_name, db_user_name, db_user_pwd):
-    print("Create mysql")
+    print("建立監控系統資料庫")
     if (os.path.isdir("./School_Monitor") == 0): os.mkdir("./School_Monitor")
     if (os.path.isdir("./School_Monitor/mysql") == 0): os.mkdir("./School_Monitor/mysql")
     os.chdir("./School_Monitor")
@@ -74,12 +78,11 @@ def create_service():
     print("資料庫服務名稱：", librenms_mysql_docker_name)
     print("資料庫總管密碼：", librenms_mysql_root_pwd)
     librenms_mysql_ip = create_mysql(librenms_mysql_docker_name, librenms_mysql_root_pwd, librenms_mysql_user_db, librenms_mysql_user_name, librenms_mysql_user_pwd)
-#    librenms_mysql_ip = "172,17,0,2"
     print(librenms_mysql_ip)
     create_librenms(librenms_docker_name, librenms_mysql_docker_name, librenms_mysql_ip, librenms_mysql_user_db, librenms_mysql_user_name, librenms_mysql_user_pwd)
 
-    user_name = input("請輸入使用者帳號：")
-    user_pwd = input("請輸入使用者密碼：")
+#    user_name = input("請輸入使用者帳號：")
+#    user_pwd = input("請輸入使用者密碼：")
 
 
 while(error_id == 1):
