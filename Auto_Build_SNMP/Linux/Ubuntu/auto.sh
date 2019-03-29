c6d0347a31b79d8015fd8042e3cf4ae846d3ff21
andy212130 <quan_xi87@yahoo.com.tw>
2019-03-29 15:51:20 +0800

    Ubuntu_Auto_SNMP_V2

--- Auto_Build_SNMP/Linux/Ubuntu/auto.sh
+++ Auto_Build_SNMP/Linux/Ubuntu/auto.sh
@@ -10,12 +10,14 @@ sudo sed -i '11c SNMPDOPTS="'"-Lsd -Lf /dev/null -u snmp -g snmp -I -smux, -p /r
 
 ##get IP
 #MY_IP=$(hostname -I) or 
-IP=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' |cut -d: -f2 | awk '{ print $1}')
+#IP=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' |cut -d: -f2 | awk '{ print $1}')
 #echo $MY_IP
 
-sudo sed -i '15c agentAddress  udp:'$IP':161' /etc/snmp/snmdp.conf
+sudo sed -i '15c agentAddress  udp:0.0.0.0:161' /etc/snmp/snmdp.conf
 sudo sed -i '49c rocommunity public  localhost ' /etc/snmp/snmpd.conf
-sudo sed -i '50c rocommunity imac-iot default' /etc/snmp/snmpd.conf
+
+                                     #librenmsIP
+sudo sed -i '50c rocommunity imac-iot 172.3.0.3' /etc/snmp/snmpd.conf
 
 sudo service snmpd restart
 
