96ac4d494b2c2e66f0c19b3675d7d53fcfe853dc
鄭鍾翔 <zhengzhongxiang@zhengzhongxiangde-MacBook-Pro.local>
2019-12-08 21:26:38 +0800

    fix mysql control

--- Server/server.py
+++ Server/server.py
@@ -2,8 +2,10 @@
 
 from flask import Flask, request
 # import MySQLdb
+import os, sys
 import docker
 import json
+
 
 # define Mysql status
 mysql_conn = ""
@@ -26,9 +28,9 @@ edge_school_container_id = ""
 
 # define mysql command
 # cloud system
-mysql_service_db = "school-monitor-system"
+mysql_service_db = "school_monitor_system"
 mysql_create_service_db = "create database " + mysql_service_db
-mysql_service_table = "edge-regist"
+mysql_service_table = "edge_regist"
 
 mysql_create_regist_table = "CREATE TABLE " + mysql_service_table + " (\
     School_Id           int AUTO_INCREMENT, \
@@ -44,28 +46,28 @@ mysql_create_edge_db = "create database " + mysql_edge_db
 mysql_edge_table = "librenms"
 
 mysql_create_status_table = "CREATE TABLE librenms (\
-    School_Id           int AUTO_INCREMENT, \
-    School_Ip           varchar(17) NOT NULL, \
-    School_MAC          varchar(17) NOT NULL, \
-    School_Port         int NOT NULL, \
-    School_ContainerId  varchar(64) NOT NULL, \
-    PRIMARY KEY(School_Id));')"
+    school_Id           int AUTO_INCREMENT, \
+    school_Ip           varchar(17) NOT NULL, \
+    school_MAC          varchar(17) NOT NULL, \
+    school_Port         int NOT NULL, \
+    school_ContainerId  varchar(64) NOT NULL, \
+    school_LastCheck    datatime NOT NULL, \
+    PRIMARY KEY(school_Id));')"
 
 mysql_push_edge_data = ""
-'''
+
 def mysql_connect():
     global mysql_conn, mysql_connection
     try:
         mysql_conn = MySQLdb.connect(host = mysql_host, \
             port=mysql_port, \
             user=mysql_user, \
-            passwd=mysql_passwd, \_
+            passwd=mysql_passwd, \
             charset="utf8")
         mysql_connection = mysql_conn.cursor()
         return True
     except:
         return False
-'''
 
 # mysql 檢查指定 db 是否存
 def mysql_check_db(dbName):
@@ -73,10 +75,10 @@ def mysql_check_db(dbName):
     if mysql_connect() == True:
         mysql_connection.execute("show databases;")
         for x in mysql_connection:
-            if x == dbName:
-                return False
-            else: 
+            if x[0] == dbName:
                 return True
+        else:
+            return False
     else:
         False
 
@@ -84,13 +86,14 @@ def mysql_check_db(dbName):
 def mysql_check_table(dbName, tableName):
     global mysql_conn, mysql_connection
     if mysql_check_db(dbName) == True:
-        mysql_connection = mysql_conn.select_db(dbName)
+        mysql_conn.select_db(dbName)
+        mysql_connection = mysql_conn.cursor()
         mysql_connection.execute("show tables;")
         for x in mysql_connection:
-            if x == tableName:
-                return False
-            else: 
+            if x[0] == tableName:
                 return True
+        else: 
+            return False
     else:
         False
 
@@ -130,10 +133,9 @@ if (not mysql_check_db(mysql_service_db)):
     mysql_connection.execute(mysql_create_service_db)
 mysql_conn.select_db(mysql_service_db)
 mysql_connection = mysql_conn.cursor()
-if (not mysql_check_table(mysql_service_db, "regist"))
-    print("create regist database")
+if (not mysql_check_table(mysql_service_db, mysql_service_table)):
+    print("create regist table")
     mysql_connection.execute(mysql_create_regist_table)
-
 
 @app.route('/listContainer', methods=['GET'])
 def listContainer():
@@ -182,12 +184,19 @@ def edgeNodeRegist():
             print("School_MAC = "+ edge_school_mac)
             print("School_ContainerId = "+ edge_school_container_id)
             if mysql_connect() == 1:
-                conn.select_db('edge-regist')
+                conn.select_db(mysql_service_db))
                 mysql_connection = conn.cursor()
-                mysql_find_school = mysql_connection.execute('Select * from school where School_Id==' + edge_school_id)
-                # if (mysql_find_school)
-                # try:
-                #    mysql_connection.excute('Insert INTO School (School_Id, School_Ip, School_MAC, School_Port, School_ContainerId) VALUE ' + edge_school_id + ", " + edge_school_ip + ", " + edge_school_mac + ", " + edge_school_port + ", " + edge_school_container_id)
+                mysql_find_school = mysql_connection.execute("Select school_Id from " + mysql_service_table + " where school_Id = " + str(edge_school_id))
+                if (mysql_find_school == 0):
+                    try:
+                        mysql_connection.excute('Insert INTO ' + mysql_service_table + ' (School_Id, School_Ip, School_MAC, School_Port, School_ContainerId, School_LastCheck) VALUE ' + str(edge_school_id) + ", " + edge_school_ip + ", " + edge_school_mac + ", " + edge_school_port + ", " + edge_school_container_id + ", " + str(datetime.datetime.now()))
+                    except: 
+                        return {"regist": "fail"}
+                else:
+                    try:
+                        mysql_connection.excute('UPDATE ' + mysql_service_table + ' SET School_Ip=' + edge_school_ip + ", School_MAC = " + edge_school_mac +", School_Port" + edge_school_port + ", School_LastCheck" + str(datetime.datetime.now() + ' WHERE School_Id = ' + edge_school_id)
+                    except: 
+                        return {"regist": "fail"}
             return {"regist": "ok"}
         except:
             return {"regist": "fail"}
