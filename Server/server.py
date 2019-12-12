d8ac076d1cfb9d1ed629a659b373997920cb48aa
鄭鍾翔 <zhengzhongxiang@zhengzhongxiangde-MacBook-Pro.local>
2019-12-13 01:55:12 +0800

    fix emmit with mysql_data data format

--- Server/server.py
+++ Server/server.py
@@ -390,12 +390,14 @@ def edgeNodeSqlUpload():
             else:
                 print("XXOO")
                 #try:
-                mysql_command = "INSERT INTO devices (device_id, hostname, sysName, ip, community, authlevel, authname, authpass, authalgo, cryptopass, cryptoalgo) VALUES (\
+                mysql_command = "INSERT INTO devices (device_id, hostname, sysName, ip, community, authlevel, authname, authpass, authalgo, cryptopass, cryptoalgo, \
+                snmpver, port, transport, timeout, retries, snmp_disable, bgpLocalAs, sysObjectID, sysDescr, sysContact, version) VALUES (\
                 " + y["device_id"] + ", " + y["hostname"] + ", " + y["sysName"] + ", " + y["ip"] + ", " + y["community"] + ", " + y["authlevel"] + ", " + y["authname"] + ", " + y["authpass"] + ", \
-                " + y["authalgo"] + ", " + y["cryptopass"] + ", " + y["cryptoalgo"] + ")"
+                " + y["authalgo"] + ", " + y["cryptopass"] + ", " + y["cryptoalgo"] + ", " + y["snmpver"] + ", " + y["port"] + ", " + y["transport"] + ", " + y["timeout"] + ", " + y["retries"] + ", \
+                " + y["snmp_disable"] + ", " + y["bgpLocalAs"] + ", " + y["sysObjectID"] + ", " + y["sysDescr"] + ", " + y["sysContact"] + ", " + y["version"] + ")"
                     
                 print(mysql_command)
-                #    snmpver, port, transport, timeout, retries, snmp_disable, bgpLocalAs, sysObjectID, sysDescr, sysContact, version, \
+                #     \
                 #    hardware, features, location_id, os, status, status_reason, ignores, disabled, uptime, agent_uptime, last_polled, \
                 #    last_poll_attempted, last_polled_timetaken, last_discovered_timetaken, last_discovered, last_ping, last_ping_timetaken, \
                 #    purpose, type, serial, icon, poller_group, override_sysLocation, notes, port_association_mode, max_depth) \
