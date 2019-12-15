f6cc401749c697af2ef67a27f0d4bcdbb9c73535
鄭鍾翔 <zhengzhongxiang@zhengzhongxiangde-MacBook-Pro.local>
2019-12-15 15:29:32 +0800

    test sqlUpload api

--- Server/server.py
+++ Server/server.py
@@ -447,7 +447,8 @@ def edgeNodeSqlUpload():
             else:
                 print("c")
                 try:
-                    mysql_connection.execute("INSERT INTO devices (device_id, hostname, sysName, ip, community, authlevel, authname, authpass, authalgo, cryptopass, cryptoalgo, \
+                    mysql_connection.execute
+                    print("INSERT INTO devices (device_id, hostname, sysName, ip, community, authlevel, authname, authpass, authalgo, cryptopass, cryptoalgo, \
                         snmpver, port, transport, timeout, retries, snmp_disable, bgpLocalAs, sysObjectID, sysDescr, sysContact, version, hardware, features, location_id, os, \
                         status, ignores, disabled, uptime, agent_uptime, last_polled, last_poll_attempted, last_polled_timetaken, last_discovered_timetaken, \
                         last_discovered, last_ping, last_ping_timetaken, purpose, type, serial, icon, poller_group, override_sysLocation, notes, port_association_mode, max_depth) \
