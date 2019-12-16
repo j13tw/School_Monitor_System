c4bbf37c1c37157acaf7a234234c2a18a99f80ae
鄭鍾翔 <zhengzhongxiang@zhengzhongxiangde-MacBook-Pro.local>
2019-12-16 21:15:56 +0800

    add device list compare

--- Server/server.py
+++ Server/server.py
@@ -468,11 +468,12 @@ def edgeNodeSqlUpload():
                     return {"uploadSql": "devices_table_insert_Error"}
             print("recive school_" + str(edge_school_id) + " devices " + y["device_id"] + "=" + y["hostname"]) 
         edge_device_list_set = set(edge_device_list)
-        print(edge_device_list_set)
+        edge_device_list = []
         mysql_connection.execute("select device_id from devices")
-        cloud_device_list_set = mysql_connection.fetchall()
+        for x in mysql_connection:
+            edge_device_list.append(x)
+        cloud_device_list_set = set(edge_device_list)
         device_difference_list = edge_device_list_set.difference(cloud_device_list_set)
-        print(cloud_device_list_set)
         print(device_difference_list)
         for x in device_difference_list:
             print(x)
