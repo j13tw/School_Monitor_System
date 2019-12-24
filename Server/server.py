a613afa46b385fc7557eec342c636a702fc07fb3
鄭鍾翔 <zhengzhongxiang@zhengzhongxiangde-MacBook-Pro.local>
2019-12-24 16:52:54 +0800

    test

--- Server/server.py
+++ Server/server.py
@@ -431,7 +431,7 @@ def edgeNodeSqlUpload():
             try:
                 mysql_connection.execute("INSERT INTO device_state_history (id, device_id, status ,time_logged) \
                 VALUES (\
-                " + str(device_state_count) + ", " + y["device_id"] + ", " + y["status"] + ", " + str(datetime.datetime.now()) + ")"
+                " + str(device_state_count) + ", " + y["device_id"] + ", " + y["status"] + ", " + str(datetime.datetime.now()) + ")")
                 mysql_conn.commit()
             except:
                 return {"uploadSql": "device_state_history_table_insert_Error"}
