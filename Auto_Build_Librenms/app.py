922554a4c6332cf67abe910f60baaf5a013e99ed
ZhengSheng <j13tw@yahoo.com.tw>
2020-01-07 17:49:59 +0800

    test docker auto build

--- Auto_Build_Librenms/app.py
+++ Auto_Build_Librenms/app.py
@@ -33,7 +33,8 @@ def create_librenms(school_serial_id, school_name, docker_mysql_name, docker_mys
     print("生成系統認證金鑰碼")
     docker_librenms_config = "docker run --rm jarischaefer/docker-librenms generate_key"
     librenms_product_key = str(subprocess.Popen(docker_librenms_config, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].decode('utf-8')).split("\n")[0]
-   # print(librenms_product_key)
+    # print(librenms_product_key)
+    if (os.path.isdir("./School_Monitor") == 0): os.mkdir("./School_Monitor")
     if (os.path.isdir("./School_Monitor/librenms") == 0): os.mkdir("./School_Monitor/librenms")
     if (os.path.isdir("./School_Monitor/librenms/" + school_name) == 0): os.mkdir("./School_Monitor/librenms/" + school_name)
     if (os.path.isdir("./School_Monitor/librenms/" + school_name + "/logs") == 0): os.mkdir("./School_Monitor/librenms/" + school_name + "/logs")
