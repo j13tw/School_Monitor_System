85246a2fd13fc82a59547bf306f585c281061e25
ZhengSheng <j13tw@yahoo.com.tw>
2020-01-09 20:43:53 +0800

    test

--- Server/server.py
+++ Server/server.py
@@ -353,10 +353,10 @@ def edgeNodeSqlUpload():
             mysql_reconnect()
             mysql_conn.select_db("school_" + edge_school_id)
             mysql_connection = mysql_conn.cursor()
-            # mysql_connection.execute("select id from device_state_history group by id desc limit 1")
-            # if (len(list(mysql_connection)) == 0): device_state_count = 0
+            device_count_id = mysql_connection.execute("select id from device_state_history group by id desc limit 1").fetchall()
+            # if (len(list(device)count_id)) == 0): device_state_count = 0
             # else: 
-            #     device_state_count = int(list(mysql_connection)[0][0])
+            #     device_state_count = int(list(device)count_id)[0][0])
 
             # edge devices table update
             print("Refresh devices tables")
