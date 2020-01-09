712c00696662484229cdb845ef6968b858f2ad86
ZhengSheng <j13tw@yahoo.com.tw>
2020-01-09 11:51:09 +0800

    server error test fix

--- Server/server.py
+++ Server/server.py
@@ -293,6 +293,10 @@ def edgeNodeRegist():
             return {"regist": "fail", "info": "post_Error"}
         
         if (mysql_connect() == True):
+            try:
+                mysql_conn.ping()
+            except:
+                mysql_reconnect()
             mysql_conn.select_db(mysql_service_db)
             mysql_connection = mysql_conn.cursor()
             mysql_check_school = mysql_connection.execute("Select school_Id from " + mysql_school_table + " where school_Id = '" + edge_school_id + "'")
@@ -349,6 +353,10 @@ def edgeNodeSqlUpload():
         edge_device_list = []
 
         if (mysql_connect() == True):
+            try:
+                mysql_conn.ping()
+            except:
+                mysql_reconnect()
             mysql_conn.select_db("school_" + edge_school_id)
             mysql_connection = mysql_conn.cursor()
             mysql_connection.execute("select id from device_state_history group by id desc limit 1")
