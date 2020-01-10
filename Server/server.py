b5a5a916290f00f8448f5e0eeb4f647697777eee
ZhengSheng <j13tw@yahoo.com.tw>
2020-01-10 16:41:41 +0800

    test

--- Server/server.py
+++ Server/server.py
@@ -307,6 +307,7 @@ def edgeNodeRegist():
             mysql_conn = MySQLdb.connect(host = mysql_host, \
                 port=mysql_port, \
                 user=mysql_user, \
+                db=mysql_service_db, \
                 passwd=mysql_passwd)
             mysql_connection = mysql_conn.cursor()
             mysql_check_school = mysql_connection.execute("Select school_Id from " + mysql_school_table + " where school_Id = '" + edge_school_id + "'")
