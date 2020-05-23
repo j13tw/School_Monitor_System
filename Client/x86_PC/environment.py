1e84edb9d7dbb59c427e179d054d370ccb1cc921
ZhengSheng <j13tw@yahoo.com.tw>
2020-05-23 22:30:36 +0800

    fix librenms error drawing graph test

--- Client/x86_PC/environment.py
+++ Client/x86_PC/environment.py
@@ -4,8 +4,7 @@ import time
 from subprocess import check_output
 
 
-os.system("export LC_ALL=\"en_US.UTF-8\"")
-os.system("export LC_CTYPE=\"en_US.UTF-8\"")
+os.system("/opt/librenms/daily.sh"")
 os.system("apt-get install -y python3 python3-dev python3-pip")
 # os.system("apt-get install -y gcc libssl-dev")
 os.system("apt-get install -y libmysqlclient-dev")
