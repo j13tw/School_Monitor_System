4d2ee66919df5d6a63581f5611f85aaec44506ec
ZhengSheng <j13tw@yahoo.com.tw>
2020-06-10 17:10:15 +0800

    add auto update test fix update time - 1

--- Client/updateCheck.py
+++ Client/updateCheck.py
@@ -9,12 +9,12 @@ preDate = 0
 while (True):
     print(str(datetime.datetime.now()), "check wating time ...")
     timeInfo = datetime.datetime.strftime(datetime.datetime.now(), "%d-%H")
-    if (preDate != timeInfo.split("-")[0]):
-        preDate = timeInfo.split("-")[0]
-        updateCheck = 0
+    # if (preDate != timeInfo.split("-")[0]):
+    #     preDate = timeInfo.split("-")[0]
+    #     updateCheck = 0
     if (timeInfo.split("-")[1] == "17" and updateCheck == 0):
         print(str(datetime.datetime.now()), "checking github online code ...")
-        updateCheck = 1
+        # updateCheck = 1
         os.chdir("/home/pi/School_Monitor_System/")
         updateInfo = subprocess.Popen(["sudo", "git", "pull"],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         updateStatus, stderr = updateInfo.communicate()
