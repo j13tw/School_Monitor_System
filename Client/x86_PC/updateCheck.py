76a0d05c000290df6a8b1c3549b90420718f75f1
ZhengSheng <j13tw@yahoo.com.tw>
2020-06-10 19:07:38 +0800

    add x86_pc auto update-2

---
+++ Client/x86_PC/updateCheck.py
@@ -0,0 +1,27 @@
+import os
+import datetime
+import subprocess
+import time
+
+updateCheck = 0
+preDate = 0
+
+while (True):
+    print(str(datetime.datetime.now()), "check wating time ...")
+    timeInfo = datetime.datetime.strftime(datetime.datetime.now(), "%d-%H")
+    if (preDate != timeInfo.split("-")[0]):
+        preDate = timeInfo.split("-")[0]
+        updateCheck = 0
+    if (timeInfo.split("-")[1] == "18" and updateCheck == 0):
+        print(str(datetime.datetime.now()), "checking github online code ...")
+        updateCheck = 1
+        os.chdir("/home/pi/School_Monitor_System/")
+        updateInfo = subprocess.Popen(["sudo", "git", "pull"],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        updateStatus, stderr = updateInfo.communicate()
+        if not (updateStatus.decode("utf-8") == "Already up to date.\n"):
+            print(str(datetime.datetime.now()), "updating online code ...")
+            os.system("python3 /home/pi/School_Monitor_System/Client/x86_PC/environment.py")
+        else:
+            print(str(datetime.datetime.now()), "nothing update ...")
+    time.sleep(60)
+    
